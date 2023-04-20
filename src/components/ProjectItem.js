1b3d29952bbc4556b869fb2f0d62d06079cb39a9
Wycliffe <wycliffekiprono4@gmail.com>
2023-04-20 14:31:56 +0300

    done

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+ //check if the required props are present
+ if (!name || !about || !Array.isArray(technologies)) {
+  return null
+ }
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((technology) => (
+          <span key={technology}>{technology}</span>
+        ))}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,26 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  //check if the projects prop is an array
+  if (!Array.isArray(projects)) {
+    return null
+  }
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+     
+      <div id="project-list">
+        {/* render one ProjectItem components with the correct props */}
+        {projects.map((project) => (
+          <ProjectItem 
+            key={project.id}
+            name={project.name}
+            about={project.about}
+            technologies={project.technologies}
+          />
+        ))} 
+      </div>
     </div>
   );
 }
